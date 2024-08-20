7d3217177b525f1d7579f0ee52379605dd4acae5
Ali <userali.ir@gmail.com>
2024-08-20 17:41:32 +0330

    Script

---
+++ usa/usa1.js
@@ -0,0 +1,5 @@
+            console.info("⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️");
+            console.info("👤 > Ali Dehghan");
+            console.info("🌐 > www.userali.ir");
+            console.info("✉️ > userali@mail.ir");
+            console.info("⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️");
